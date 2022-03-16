108cefa8d87c3f99cadfda78530cd63214e11974
Isaac <isaacwildsecond@gmail.com>
2022-03-16 16:28:36 +0000

    calculator working and correct

--- script.js
+++ script.js
@@ -28,11 +28,12 @@ if(operator == '+'){
 }
 
 }
-// need to add in DOM stuff to interact with buttons.
+
 let num1 = null;
 let num2 = null;
 let operator = null;
 let sum = null;
+let chain = false;
 
 const display = document.querySelector(".display");
 const btn1 = document.querySelector("#button1");
@@ -53,165 +54,230 @@ const btnEqual = document.querySelector("#equalsButton");
 const btnClear = document.querySelector("#buttonClear");
 const btnDot = document.querySelector("#buttonDot");
 
-//add event listener
 btn1.addEventListener("click", () => {
-if(sum !== null)
-{
-//add toggle in here to allow multiple numbers in chaining calcs
-display.textContent = "";
-updateDisplay(1);
-}
-updateDisplay(1);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(1);
+        chain = false;
+    }else
+        updateDisplay(1);
 });
+
 btn2.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(2);
-}else
-updateDisplay(2);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(2);
+        chain = false;
+    }else
+        updateDisplay(2);   
 });
+
 btn3.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(3);
-}else
-updateDisplay(3);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(3);
+        chain = false;
+    }else
+        updateDisplay(3);
 });
+
 btn4.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(4);
-}else
-updateDisplay(4);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(4);
+        chain = false;
+    }else
+        updateDisplay(4);
 });
+
 btn5.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(5);
-}else
-updateDisplay(5);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(5);
+        chain = false;
+    }else
+        updateDisplay(5);
 });
+
 btn6.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(6);
-}else
-updateDisplay(6);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(6);
+        chain = false;
+    }else
+        updateDisplay(6);
 });
+
 btn7.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(7);
-}else
-updateDisplay(7);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(7);
+        chain = false;
+    }else
+        updateDisplay(7);
 });
+
 btn8.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(8);
-}else
-updateDisplay(8);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(8);
+        chain = false;
+    }else
+        updateDisplay(8);
 });
+
 btn9.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(9);
-}else
-updateDisplay(9);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(9);
+        chain = false;
+    }else
+        updateDisplay(9);
 });
+
 btn0.addEventListener("click", () => {
-if(sum !== null)
-{
-display.textContent = "";
-updateDisplay(0);
-}else
-updateDisplay(0);
+    if(sum !== null && chain == true)
+    {
+        display.textContent = "";
+        updateDisplay(0);
+        chain = false;
+    }else
+        updateDisplay(0);
 });
+
 btnAdd.addEventListener("click", () => {
-if(operator !== null){
-chainCalc();
-operator = "+"
-}else if(operator == null){
-operator = "+"
-num1 = parseFloat(display.textContent)
-display.textContent = "";
-}
+    clearButtons();
+    buttonSelected(btnAdd);
+    if(operator !== null){
+        chainCalc();
+        operator = "+"
+    }else if(operator == null){
+        operator = "+"
+        num1 = parseFloat(display.textContent)
+        display.textContent = "";
+    }
 });
+
 btnSub.addEventListener("click", () => {
-if(operator !== null){
-chainCalc();
-operator = "-"
-}else if(operator == null){
-operator = "-"
-num1 = parseFloat(display.textContent)
-display.textContent = "";
-}
+    clearButtons();
+    buttonSelected(btnSub);
+    if(operator !== null){
+        chainCalc();
+        operator = "-"
+    }else if(operator == null){
+        operator = "-"
+        num1 = parseFloat(display.textContent)
+        display.textContent = "";
+    }
 });
+
 btnMult.addEventListener("click", () => {
-if(operator !== null){
-chainCalc();
-operator = "*"
-}else if(operator == null){
-operator = "*"
-num1 = parseFloat(display.textContent)
-display.textContent = "";
-} 
+    clearButtons();
+    buttonSelected(btnMult);
+    if(operator !== null){
+        chainCalc();
+        operator = "*"
+    }else if(operator == null){
+        operator = "*"
+        num1 = parseFloat(display.textContent)
+        display.textContent = "";
+    } 
 });
+
 btnDiv.addEventListener("click", () => {
-if(operator !== null){
-chainCalc();
-operator = "/"
-}else if(operator == null){
-operator = "/"
-num1 = parseFloat(display.textContent)
-display.textContent = "";
+    clearButtons();
+    buttonSelected(btnDiv);
+    if(operator !== null){
+        chainCalc();
+        operator = "/"
+    }else if(operator == null){
+    operator = "/"
+    num1 = parseFloat(display.textContent)
+    display.textContent = "";
 }
 });
+
 btnEqual.addEventListener("click", () => {
-calc();
+        calc();
 });
+
 btnClear.addEventListener("click", () => {
-num1 = null;
-num2 = null;
-operator = null;
-sum = null;
-display.textContent = "";
+    num1 = null;
+    num2 = null;
+    operator = null;
+    sum = null;
+    display.textContent = "";
+    clearButtons();
 });
+
 btnDot.addEventListener("click", () => {
-updateDisplay(".");
+    updateDisplay(".");
+    disableDot();
 });
 
 function updateDisplay(x) {0
-if(display.textContent.length < 14){
-display.textContent = display.textContent + x;
-}else
-return;
+    if(display.textContent.length < 14){
+        display.textContent = display.textContent + x;
+    }else
+    return;
 }
 
 function calc() {
-num2 = parseFloat(display.textContent)
-if(num1 !== null && operator !== null && num2 !==null){
-operate(num1,operator,num2);
-display.textContent = sum;
-num1 = sum;
-num2 = null;
-operator = null;
-}else
-return;
+    num2 = parseFloat(display.textContent)
+    if(num1 !== null || operator !== null || num2 !==null){
+        operate(num1,operator,num2);
+        display.textContent = sum;
+        num1 = sum;
+        num2 = null;
+        operator = null;
+        clearButtons();
+        chain = true;
+    }else
+    return;
 }
 function chainCalc() {
-num2 = parseFloat(display.textContent)
-operate(num1,operator,num2);
-display.textContent = sum;
-num1 = sum;
-num2 = null;
+    num2 = parseFloat(display.textContent)
+    operate(num1,operator,num2);
+    display.textContent = sum;
+    num1 = sum;
+    num2 = null;
+    chain = true;
+}
 
+function clearButtons(){
+    btnAdd.classList.remove("buttonSelected");
+    btnAdd.classList.add("button");
+    btnSub.classList.remove("buttonSelected");
+    btnSub.classList.add("button");
+    btnMult.classList.remove("buttonSelected");
+    btnMult.classList.add("button");
+    btnDiv.classList.remove("buttonSelected");
+    btnDiv.classList.add("button");
+    btnDot.classList.remove("buttonSelected")
+    btnDot.classList.add("button")
+    enableDot();
+}
+
+function buttonSelected(x){
+    (x).classList.add("buttonSelected");
+    (x).classList.remove("button");
+}
+
+function disableDot(){
+    btnDot.disabled = true;
+    btnDot.classList.add("buttonSelected")
+    btnDot.classList.remove("button")
+}
+
+function enableDot(){
+    btnDot.disabled = false;
 }
